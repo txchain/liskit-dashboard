d4a4ea8fc02c86686f418a9786356e8d9ce18ece
Andrea Francesco Speziale <andreaf.speziale@gmail.com>
2016-05-06 17:10:33 +0200

    update

--- public/src/dashboard/config/dashboard.client.routes.js
+++ public/src/dashboard/config/dashboard.client.routes.js
@@ -1,10 +1,12 @@
 /**
  * Created by andreafspeziale on 13/04/16.
  */
-dashboard.config(['$routeProvider', function($routeProvider){
+dashboard.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
     $routeProvider.when('/', {
          templateUrl: '/public/src/dashboard/views/dashboard.html'
        }).otherwise({
       redirectTo: '/'
     });
+    // use the HTML5 History API
+    $locationProvider.html5Mode(true);
 }])
--- public/src/dashboard/dashboard.client.module.js
+++ public/src/dashboard/dashboard.client.module.js
@@ -1,1 +1,1 @@
-var dashboard = angular.module('dashboard', ['ngAnimate', 'ui.bootstrap']);
+var dashboard = angular.module('dashboard', ['ngAnimate', 'ui.bootstrap', 'duScroll']);
