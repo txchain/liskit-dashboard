07860cbf09345092a7716d5660fdaa5655017b0a
Andrea Francesco Speziale <andreaf.speziale@gmail.com>
2016-06-15 21:58:45 +0200

    added new detail angular mini app

---
+++ public/src/detail/controllers/detail.client.controller.js
@@ -0,0 +1,5 @@
+detail.controller('DetailController', ['$scope',
+    function($scope) {
+        console.log("DETAIL CONTROLLER");
+        $scope.name_controller = 'DetailController';
+    }]);
---
+++ public/src/detail/detail.client.module.js
@@ -0,0 +1,1 @@
+var detail = angular.module('detail', []);
