b94d143b20e29b8cd00267c3a1072d3843672ed1
andreafspeziale <andreaf.speziale@gmail.com>
2017-05-03 19:20:23 +0200

    useless backend service func removed

--- public/src/backend/services/backend.client.service.js
+++ public/src/backend/services/backend.client.service.js
@@ -20,39 +20,6 @@ backend.factory('BackendServices', ['$http', '$q', function($http, $q) {
                     console.log('getForgingInfo lisk service promise rejected');
                     return $q.reject(response.data);
                 });
-        }/*,
-        getTransactions: function (recipient) {
-            console.log(recipient)
-            return $http.get(ip + '/aaapi/transactions?senderId=' + myAddress + '&AND:recipientId=' + recipient + '&orderBy=timestamp:desc')
-                .then(function(response) {
-                    if (typeof response.data === 'object') {
-                        console.log(response.data);
-                        return response.data;
-                    } else {
-                        console.log('getTransactions lisk service invalid response from API');
-                        return $q.reject(response.data);
-                    }
-
-                }, function(response) {
-                    console.log(response)
-                    console.log('getTransactions lisk service promise rejected');
-                    return $q.reject(response.data);
-                });
-        }*/
-            /*
-            return $http.get(ip+'/api/getForgingInfo?address='+address)
-                .then(function(response) {
-                    if (typeof response.data === 'object') {
-                        return response.data;
-                    } else {
-                        console.log('getBalance lisk service invalid response from API');
-                        return $q.reject(response.data);
-                    }
-
-                }, function(response) {
-                    console.log('getBalance lisk service promise rejected');
-                    return $q.reject(response.data);
-                });
-        }*/
+        }
     }
 }]);
