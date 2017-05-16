6f0fdd40a7259a8936737250f66da44e54d142cd
andreafspeziale <andreaf.speziale@gmail.com>
2017-05-16 13:31:14 +0200

    my pool config

--- public/src/env/services/env.client.service.js
+++ public/src/env/services/env.client.service.js
@@ -1,8 +1,8 @@
 env.factory('EnvServices', [function() {
     return {
-        poolAddress: '14621643025887137539L',
-        backEndIp: 'http://194.116.72.33:5000',
-        openApiNodeIp:'http://194.116.72.33:7000',
+        poolAddress: '10310263204519541551L',
+        backEndIp: 'http://194.116.72.47:5000',
+        openApiNodeIp:'http://liskworld.info:8000/',
         dynamicPool: false,
 
         // make attention that 100 - maintenancePerc -  topXXperc it's > 0
@@ -13,6 +13,6 @@ env.factory('EnvServices', [function() {
 
         maintenancePerc: 30,
 
-        swapHolding: 0
+        swapHolding: 60000
     }
 }]);
