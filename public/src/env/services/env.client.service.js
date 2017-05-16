0a2956792844f2b48e4a475e8d3f1c245d949a1d
andreafspeziale <andreaf.speziale@gmail.com>
2017-05-16 13:32:29 +0200

    erro fix in config

--- public/src/env/services/env.client.service.js
+++ public/src/env/services/env.client.service.js
@@ -2,7 +2,7 @@ env.factory('EnvServices', [function() {
     return {
         poolAddress: '10310263204519541551L',
         backEndIp: 'http://194.116.72.47:5000',
-        openApiNodeIp:'http://liskworld.info:8000/',
+        openApiNodeIp:'http://liskworld.info:8000',
         dynamicPool: false,
 
         // make attention that 100 - maintenancePerc -  topXXperc it's > 0
